7d1f01a9640ece3f643d5aa2db32b8392b1422c1
sean <hyunsik-park@textory.com>
2018-04-02 15:38:58 +0900

    to apply rebeccapurple.communicator.Task for standard

--- src/main/java/rebeccapurple/http/Task.java
+++ src/main/java/rebeccapurple/http/Task.java
@@ -1,4 +1,4 @@
 package rebeccapurple.http;
 
-public interface Task extends rebeccapurple.communicator.Task<Request>, rebeccapurple.Request<Request, Response> {
+public interface Task extends rebeccapurple.communicator.Task<Message>, rebeccapurple.Request<Message, Message> {
 }
