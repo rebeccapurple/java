677ad9345b590dd3fd2b0ca7a813e2135bb3828e
sean <hyunsik-park@textory.com>
2018-04-03 20:09:27 +0900

    resource.

--- src/main/java/rebeccapurple/Resource.java
+++ src/main/java/rebeccapurple/Resource.java
@@ -1,6 +1,22 @@
 package rebeccapurple;
 
-public interface Resource {
-    Identifier local();
-    Identifier remote();
+public class Resource {
+    private Identifier __local;
+    private Identifier __remote;
+
+    public Identifier local(){ return __local; }
+    public Identifier remote(){ return __remote; }
+
+    public void local(Identifier v){ __local = v; }
+    public void remote(Identifier v){ __remote = v; }
+
+    public Resource(){
+        __local = null;
+        __remote = null;
+    }
+
+    public Resource(Identifier local, Identifier remote){
+        __local = local;
+        __remote = remote;
+    }
 }
