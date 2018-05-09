58bbe17817cc4fa81c3e57d86a9b5c03030423b1
sean <hyunsik-park@textory.com>
2018-05-09 12:23:21 +0900

    add rebeccapurple.exception.InvalidQueryException

---
+++ src/main/java/rebeccapurple/exception/InvalidQueryException.java
@@ -0,0 +1,11 @@
+package rebeccapurple.exception;
+
+public class InvalidQueryException extends Throwable {
+    public InvalidQueryException(){
+
+    }
+
+    public InvalidQueryException(Throwable cause){
+        super(cause);
+    }
+}
